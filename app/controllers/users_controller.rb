9ac4d3c08d1cad93e50b195716d3209f17175796
McCartney Bohn <mccartneybohn@yahoo.com>
2015-02-13 11:43:32 -0700

    added where users can see posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+
   def new
-    user = User.new
+    @user = User.new
+  end
+  def show
+    @users =User.all
   end
 
   def create
-    user = User.new
+    @user = User.new
     user = User.create
     user.save
   end
